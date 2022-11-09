71a73d6e43c1f679f4f546410a75d4d25bf1dc63
JuliusNguyen <julius.reischauer@googlemail.com>
2022-11-09 22:10:48 +0100

    replace console library project with windows forms project

---
+++ Installer/Installer.cs
@@ -0,0 +1,15 @@
+namespace Installer
+{
+    public partial class Installer : Form
+    {
+        public Installer()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
